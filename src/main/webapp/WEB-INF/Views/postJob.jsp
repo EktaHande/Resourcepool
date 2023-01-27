132b15c423abffe1a467fa846dab9475a33b329f
satish-chaurasiya <chaurasiyasatish717@gmail.com>
2023-01-27 17:47:54 +0530

    search skill tab

--- src/main/webapp/WEB-INF/Views/postJob.jsp
+++ src/main/webapp/WEB-INF/Views/postJob.jsp
@@ -6,7 +6,7 @@
 			<form id="postJob" action="#" method="post" class="">
 				<div class="tab container pt-3" style="height: 600px">
 					<div class="alert border" role="alert">
-						<i class="ri-award-fill mb-0 pb-0 me-4"></i>Just a reminder to publish your job post , you will need to <a href="">varify your phone number</a>
+						<i class="ri-award-fill mb-0 pb-0 me-4"></i>Just a reminder to publish your job post , you will need to <a href="">verify your phone number</a>
 					</div>
 					<div class="row">
 						<div class="col-6 d-flex justify-content-center border-end">
@@ -22,17 +22,17 @@
 						<div class="col-6 d-flex justify-content-center p-3">
 							<div class="w-75">
 								<div class="m-0 p-0">
-									<label for="jobHeadline" class="mb-3 fw-bold">Write headline for your job post</label> <input type="text" class="border-success form-control"
+									<label for="jobHeadline" class="mb-3 fw-bold">Write headline for your job post</label> <input type="text" class=" border-success form-control"
 										name="jobHeadline" id="jobHeadline" /> <label for="jobHeadline" class="error text-danger"></label>
-									<div class="headline-confirm mb-3">
+									<div class="headline-confirm my-3">
 										<p class="text-success">We'll match you with candidates that specialize in this field</p>
 									</div>
 									<p class="fw-bold">Example titles</p>
 									<!-- Some borders are removed -->
 									<ul class="text-justify">
 										<li class="mb-2">Build responsive Wordpress site with bootstrap/Payment functionality</li>
-										<li class="mb-2">Build responsive Wordpress site with bootstrap/Payment functionality</li>
-										<li class="mb-2">Build responsive Wordpress site with bootstrap/Payment functionality site with bootstrap/Payment functionality</li>
+										<li class="mb-2">AR experience needed for vitual product demos(ARCores)</li>
+										<li class="mb-2">Developer needed to update Android app UI for new OS/device specs</li>
 									</ul>
 								</div>
 							</div>
@@ -54,15 +54,14 @@
 							<div class="w-75">
 								<div class="p-0 post-job-skill">
 									<label for="postjobSkill" class="mb-3 fw-bold">Search Skills or add your own</label> 
-									<div class="">
-									<select class="form-select skill-post-select"
-										name="postjobSkill" style="width: 100% !important;" id="postjobSkill" multiple="multiple">
-									</select>
+									<div class="search-container">
+										<input type="text" name="skillSearch" id="skillSearch"  class="ps-5 form-control border-success mb-3" />
+										<em class="ri-search-line skill-search"></em>
 									</div>
+									<label for="" class="text-success ps-1" ><em class="p-1 ri-star-fill text-light bg-success border me-2 rounded-circle"></em>For thr best result, add 3-5 skills</label>
 									<label for="postjobSkill" class="error text-danger"></label>
 									<div class="selected-div">
-										<p class="text-secondary mt-2">Selected Skills</p>
-										<div class="foo"></div>
+										
 									</div>
 									<h6 class="my-3 fw-bold">Popular skills for Web Design</h6>
 									<div class="addition-skill-container d-flex flex-wrap"></div>
@@ -75,7 +74,7 @@
 				<div class="tab"><p class="fw-bold heading-title tab-title">new2</p></div>
 				<div style="overflow: auto;">
 					<div class="d-flex justify-content-between">
-						<button type="button" class="btn btn-outline-dark rounded-pill" id="prevJobBtn" onclick="nextSkill(-1)">Previous</button>
+						<button type="button" class="btn btn-outline-success rounded-pill" id="prevJobBtn" onclick="nextSkill(-1)">Previous</button>
 						<button type="button" class="btn btn-outline-success rounded-pill next" id="nextJobBtn" onclick="nextSkill(1)">Next : Skills</button>
 					</div>
 				</div>
--- target/classes/static/assets/js/postJob.js
+++ target/classes/static/assets/js/postJob.js
@@ -26,50 +26,6 @@ $("#postJob").validate({
 	}
 })
 
-let CustomSelectionAdapterForSkill = $.fn.select2.amd.require("select2/selection/customSelectionAdapter");
-
-//$('#postjobSkill').on("change" , function(){	
-
-//})
-
-var optionFromAjax = [
-    {
-        id: 0,
-        text: 'java'
-    },
-    {
-        id: 1,
-        text: 'Ajax'
-    },
-    {
-        id: 2,
-        text: '.net'
-    },
-    {
-        id: 3,
-        text: 'c#'
-    },
-    {
-        id: 4,
-        text: 'python'
-    } ,
-    {
-		id : 5 ,
-		text : 'c++' 
-	}
-];
-
-
-$("#postjobSkill").select2({
-	placeholder: "Search Skills",
-	selectionAdapter: CustomSelectionAdapterForSkill,
-	selectionContainer: $('.foo'),
-	theme: "bootstrap",
-	minimumInputLength: 3,
-	data : optionFromAjax ,
-});
-
-
 /**Json for extra option 
  */
 let extraOption = [
@@ -104,83 +60,148 @@ let extraOption = [
 	{
 		id: 13,
 		text: 'Search Engine Optimization'
-	} ,
+	},
 	{
 		id: 14,
 		text: 'Wix'
-	} ,
+	},
 	{
 		id: 15,
 		text: 'GoDaddy'
-	} ,
+	},
 	{
 		id: 16,
 		text: 'Mockup'
-	} ,
+	},
 	{
 		id: 17,
 		text: 'Layout Design'
-	} ,
+	},
 	{
 		id: 18,
 		text: 'phpMyAdmin'
-	} ,
+	},
 	{
 		id: 19,
 		text: 'Microsoft SQL Server Service Broker'
-	} ,
+	},
 	{
 		id: 20,
 		text: 'HTML 5 Canvas'
-	} ,
+	},
 	{
 		id: 21,
 		text: 'Database Administration'
-	} ,
+	},
 	{
 		id: 22,
 		text: 'Accross Language Server'
-	} ,
+	},
 	{
 		id: 23,
 		text: 'UI/UX Design Deliverables'
 	}
 ];
 
-
+//generates the extra option 
 $.each(extraOption, function(indexInArray, valueOfElement) {
 	$(".addition-skill-container").append(`
 	 <div class="badge-container d-inline-flex badge rounded-pill tags">
-	 	<span id="${indexInArray}" class="append-skill">${valueOfElement['text']}</span>
+	 	<span id="${indexInArray}" class="append-skill ${valueOfElement['text']}">${valueOfElement['text']}</span>
 	 	<em class="ri-add-line ms-2"></em>
+	 	<em class="bx bx-minus ms-2"></em>
 	 </div>
 	 `);
 });
 
-let finalValueOfSkill = [];
+//function to add skill from extra option
 $(document).on("click", ".append-skill", function() {
 	let tagName = $(this).text();
-	let optionVal = $("option");
-	let isExist = true;
-	$.each(optionVal, function(index, element) {
-		if ($(element).val() == tagName) {
-			isExist = false;
-		}
-	});
-	if (isExist) {
-		$(".skill-post-select").append(`<option value="${tagName}">${tagName}</option>`);
-	}
-	finalValueOfSkill = $("#postjobSkill").val();
-
-	console.log(finalValueOfSkill.includes($(this).text()));
-	if (finalValueOfSkill.includes($(this).text())) {
-		let index = finalValueOfSkill.indexOf($(this).text());
-		finalValueOfSkill.splice(index, 1);
+	if (!allSkill.includes(tagName)) {
+		$(this).parent().find('.ri-add-line').hide();
+		$(this).parent().find('.bx-minus').show();
+		pushTag(tagName);
 	} else {
-		finalValueOfSkill.push($(this).text());
+		let index = allSkill.indexOf(tagName);
+		$(this).parent().find('.ri-add-line').show();
+		$(this).parent().find('.bx-minus').hide();
+		allSkill.splice(index, 1);
+		generateAllTag() ;
 	}
+});
 
-	$("#postjobSkill").val(finalValueOfSkill);
-	$("#postjobSkill").trigger('change.select2');
-	console.log($("#postjobSkill").val())
+
+//initialize autocomplete 
+
+let allSkill = [];
+
+$("#skillSearch").autocomplete({
+	source: ["c++", "java", "php", "coldfusion", "asp", "ruby" ,  "asp.net"  , "softskill"],
+	minLength: 3,
+	select: function(event, ui) {
+		pushTag($("#skillSearch").val())
+	},
+	close : function(){
+		$("#skillSearch").val('') ;
+	}
+});
+
+
+
+//pushesh the tag inside array if not available
+function pushTag(skill) {
+	if (!allSkill.includes(skill)) {
+		allSkill.push(skill);
+		generateAllTag() ;
+	}
+}
+
+//generate all tags
+function generateAllTag() {
+	$(".selected-div").html(`
+		<p class="text-secondary my-2">Selected skills</p>
+										<div class="foo">
+											
+										</div>
+		`);
+	$.each(allSkill, function(indexInArray, valueOfElement) {
+		$(".foo").append(`
+			  <div class="skill-container d-inline-flex badge rounded-pill my-2">
+				 	<span class="append-skill" id="${indexInArray}">${valueOfElement}</span>
+				 	<em class="ri-close-fill remove-tag"></em>
+			  </div>
+	 	`);
+	});
+}
+
+//removes the tag 
+$(document).on("click", ".remove-tag", function() {
+	let index = allSkill.indexOf($(this).prev().text());
+	allSkill.splice(index, 1);
+	console.log(allSkill); 
+	let badgeName = $(this).prev().text() ;
+	let badge = $(".badge")
+	console.log(badge) ;
+	$.each(badge , function(index , val){
+		if($(this).find('span').text() == badgeName){
+			console.log("found equal")
+			$(badge[index]).find(".ri-add-line").show();
+			$(badge[index]).find(".bx-minus").hide();	
+		}
+	})
+	console.log(allSkill);
+	$(this).parent().remove();
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
